8fa61e2d54d7b94a64ad1bd6152b377870f1ebd3
thom033 <Thomis.rasolomandimby@gmail.com>
2025-01-19 18:26:33 +0300

    Adding reset

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -1,13 +1,18 @@
-using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Drawing;
 using System.Data;
-using System.Windows.Forms;
 using Npgsql;
+
 
 namespace Foot
 {
     public class ScoreDisplay : Form
     {
         private DataGridView dataGridView;
+        private Button resetButton;
 
         public ScoreDisplay()
         {
@@ -18,6 +23,7 @@ namespace Foot
         private void InitializeComponent()
         {
             this.dataGridView = new DataGridView();
+            this.resetButton = new Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             
@@ -25,16 +31,28 @@ namespace Foot
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Dock = DockStyle.Fill;
+            this.dataGridView.Dock = DockStyle.Top;
             this.dataGridView.Location = new System.Drawing.Point(0, 0);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView.Size = new System.Drawing.Size(800, 400);
             this.dataGridView.TabIndex = 0;
-            
+
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(350, 410);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(100, 30);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset Scores";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+
             // 
             // ScoreDisplay
             // 
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resetButton);
             this.Controls.Add(this.dataGridView);
             this.Name = "ScoreDisplay";
             this.Text = "Team Scores";
@@ -59,7 +77,29 @@ namespace Foot
             }
         }
 
-        public void Show()
+        private void Reset()
+        {
+            DBConnection dbConnection = new DBConnection();
+            string query = "DELETE FROM valiny";
+
+            using (NpgsqlConnection conn = dbConnection.GetConnection())
+            {
+                conn.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand(query, conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            LoadData();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        public new void Show()
         {
             this.ShowDialog();
         }
